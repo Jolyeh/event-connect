29627e3a3b4f2e0ec41cde2afe6ec5b8259e0bdb
Jolyeh <yehezekocolojoelprince@gmail.com>
2026-03-14 03:04:34 +0100

    fix:isLogin bug2

--- src/lib/utils/api_url.js
+++ src/lib/utils/api_url.js
@@ -1,5 +1,5 @@
-export const apiUrl = "http://localhost:3003/api";
-export const imageUrl = "http://localhost:3003";
+/* export const apiUrl = "http://localhost:3003/api";
+export const imageUrl = "http://localhost:3003"; */
 
-//export const apiUrl = "https://test-jolyeh.netiify.cloud/api";
-//export const imageUrl = "https://test-jolyeh.netiify.cloud";
+export const apiUrl = "https://test-jolyeh.netiify.cloud/api";
+export const imageUrl = "https://test-jolyeh.netiify.cloud";
